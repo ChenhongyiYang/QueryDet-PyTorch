a502777117c5efd1f373b72c7983031c53b383b1
Chenhongyi Yang <chenhongyiyang@Chenhongyis-MacBook-Pro.local>
2022-11-20 20:39:07 +0000

    fix visdrone data converting bug

--- visdrone/data_prepare.py
+++ visdrone/data_prepare.py
@@ -86,15 +86,16 @@ def label_to_coco(label, id, img_id):
 def make_json(images, annotations, new_label_json):
     ann_dict = {}
     ann_dict['categories'] = [
-        {'supercategory': 'things', 'id': 1, 'name': 'people'},
-        {'supercategory': 'things', 'id': 2, 'name': 'bicycle'},
-        {'supercategory': 'things', 'id': 3, 'name': 'car'},
-        {'supercategory': 'things', 'id': 4, 'name': 'van'},
-        {'supercategory': 'things', 'id': 5, 'name': 'truck'},
-        {'supercategory': 'things', 'id': 6, 'name': 'tricycle'},
-        {'supercategory': 'things', 'id': 7, 'name': 'awning-tricycle'},
-        {'supercategory': 'things', 'id': 8, 'name': 'bus'},
-        {'supercategory': 'things', 'id': 9, 'name': 'motor'}
+        {'supercategory': 'things', 'id': 1, 'name': 'pedestrian'},
+        {'supercategory': 'things', 'id': 2, 'name': 'people'},
+        {'supercategory': 'things', 'id': 3, 'name': 'bicycle'},
+        {'supercategory': 'things', 'id': 4, 'name': 'car'},
+        {'supercategory': 'things', 'id': 5, 'name': 'van'},
+        {'supercategory': 'things', 'id': 6, 'name': 'truck'},
+        {'supercategory': 'things', 'id': 7, 'name': 'tricycle'},
+        {'supercategory': 'things', 'id': 8, 'name': 'awning-tricycle'},
+        {'supercategory': 'things', 'id': 9, 'name': 'bus'},
+        {'supercategory': 'things', 'id': 10, 'name': 'motor'}
     ]
     ann_dict['images'] = images
     ann_dict['annotations'] = annotations
